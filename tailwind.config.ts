4b28f8704d315e260a327d7a9587725ca702e478
Osatohanmwen Solomon <solomonosato18@gmail.com>
2023-10-13 12:28:51 +0100

    Initial commit. A setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          orange: "hsl(35, 77%, 62%)",
+          red: "hsl(5, 85%, 63%)",
+        },
+        neutral: {
+          offwhite: "hsl(36, 100%, 99%)",
+          grayBlue: "hsl(233, 8%, 79%)",
+          dGrayBlue: "hsl(236, 13%, 42%)",
+          vdGrayBlue: "hsl(240, 100%, 5%)",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
